8d7bf95ab8030b08387fbce5ecf08d7eb091bb05
JulieXie1111 <titovayulia96@yandex.com>
2022-03-01 00:23:53 +1000

    Update task_2_b.py
    решила найти сумму цифр в числе другим способом.

--- task_2_b.py
+++ task_2_b.py
@@ -6,8 +6,11 @@ my_list = [i ** 3 for i in range(1, 1001) if i % 2 != 0]
 nums_seven_sum = 0
 for number in my_list:
     item_sum = 0
-    for item in str(number):
-        item_sum += int(item)
+    number_while = number
+    while number_while > 0:
+        digit = number_while % 10
+        item_sum += digit
+        number_while = number_while // 10
     if item_sum % 7 == 0:
         nums_seven_sum += number
 
@@ -17,9 +20,12 @@ nums_seventeen_seven_sum = 0
 for number in my_list:
     number += 17
     item_sum = 0
-    for item in str(number):
-        item_sum += int(item)
+    number_while = number
+    while number_while > 0:
+        digit = number_while % 10
+        item_sum += digit
+        number_while = number_while // 10
     if item_sum % 7 == 0:
-        nums_seventeen_seven_sum += int(number)
+        nums_seventeen_seven_sum += number
 
 print(nums_seventeen_seven_sum)
